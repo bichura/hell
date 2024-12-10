33f8b441a78f472402ea49a7b8bdf8876fcd4ac0
sh1dy <sh1dy.yl9@gmail.com>
2024-12-11 01:45:50 +0500

    html

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -2,8 +2,6 @@ package ru.kata.spring.boot_security.demo.configs;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -14,18 +12,16 @@ import java.util.List;
 
 @Component
 public class SuccessUserHandler implements AuthenticationSuccessHandler {
-    private static final Logger logger = LoggerFactory.getLogger(SuccessUserHandler.class);
 
     // Spring Security использует объект Authentication, пользователя авторизованной сессии.
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         List<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities()).stream().toList();
-        logger.info("User authenticated with roles: {}", roles);
-        if (roles.contains("ROLE_USER") && !request.getRequestURI().equals("/user")) {
-            logger.info("Redirecting to /user");
+        if (roles.contains(("ROLE_ADMIN"))) {
+            response.sendRedirect("/admin");
+        } else if (roles.contains("ROLE_USER")) {
             response.sendRedirect("/user");
         } else {
-            logger.info("Redirecting to /");
             response.sendRedirect("/");
         }
     }
